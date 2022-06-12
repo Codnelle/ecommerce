5493971112dbfcbab5cf4a0351f07f92d60103c3
Kashish Sharma <kashish6011@gmail.com>
2022-06-13 02:23:43 +0530

    Initial Commit

---
+++ ecommerce/ecommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecommerceApp.swift
+//  ecommerce
+//
+//  Created by Kashish Sharma on 13/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ecommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
